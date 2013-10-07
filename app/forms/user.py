f38df3650ac7b81da53d2340473fd8a3dd443e01
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-10-06 21:44:36 -0400

    form and error flashing implemented

--- app/forms/user.py
+++ app/forms/user.py
@@ -4,10 +4,13 @@ from wtforms import Form
 from wtforms import StringField, PasswordField
 from wtforms import validators
 
+# labels for form classes must be the same as the templates label since they
+# are used by some function to populate the for tag of some elements in forms.
+
 class User(Form):
-    username = StringField('Your username', [validators.Length(min=4)])
-    password = PasswordField('Your password', [validators.Length(min=8)])
-    email = StringField('Your email', [validators.Email()])
+    username = StringField('username', [validators.Length(min=4)])
+    password = PasswordField('password', [validators.Length(min=8)])
+    email = StringField('email', [validators.Email()])
 
 
 
