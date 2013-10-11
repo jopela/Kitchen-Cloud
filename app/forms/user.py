e0fb04beb7f8c5f46e0a5e1428e9f433b2d20e2c
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-10-10 21:20:28 -0400

    avancement implementation validation user

--- app/forms/user.py
+++ app/forms/user.py
@@ -3,6 +3,14 @@
 from wtforms import Form
 from wtforms import StringField, PasswordField
 from wtforms import validators
+from wtforms import ValidationError
+# Seriously, I do not understand why this work.
+from models import user
+
+# messing with the python path ...
+import sys
+
+user.create_user()
 
 # labels for form classes must be the same as the templates label since they
 # are used by some function to populate the for tag of some elements in forms.
@@ -11,6 +19,12 @@ class User(Form):
     username = StringField('username', [validators.Length(min=4)])
     password = PasswordField('password', [validators.Length(min=8)])
     email = StringField('email', [validators.Email()])
+
+    # custom validators.
+
+    def validate_username(self, username):
+        print "tryring to validate"
+        return
 
 
 
--- app/models/user.py
+++ app/models/user.py
@@ -3,14 +3,24 @@
 # This is the sqlite3 version of the functions. A database switch means a
 # function rewrite.
 
+def by_username(username):
+    """ fetch a user in the database by it's username. """
+
+    return None
+
+def by_email(email):
+    """ fetch a user from the database by it's email. """
+
+    return None
+
+
+# Should this raise an exception if fails or return true or
+# false? do not know !
 def create_user():
     """ creates a user in the database given a username, a password and an
     email address. """
     print "please implement create_user"
 
-def get_user():
-    """ fetch a user from the database given it's username or it's email
-    address. """
-    print "please implement get_user"
+
 
 
